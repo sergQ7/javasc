31187903b4e4d5c7eb21c4752304d231a31d743a
Sergei <sgorishn@gmail.com>
2024-07-31 10:41:23 +0300

    commit

---
+++ filter.js
@@ -0,0 +1,3 @@
+function filter_list(l) {
+return arr.filter(item => typeof item === 'number');
+}
---
+++ sum.js
@@ -0,0 +1,6 @@
+const sum = (a, b) => {
+   const result = a + b;
+   return result;
+} ;
+const amount = sum (2, 4);
+console.log(amount);
